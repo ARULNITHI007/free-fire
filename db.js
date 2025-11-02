d5860f7a8698c10bf39af08177f569195d54c2e5
ARULNITHI007 <arulcsa22015@gmail.com>
2025-11-02 18:21:28 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,21 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("❌ DB Connection failed:", err);
+  } else {
+    console.log("✅ Connected to MySQL");
+  }
+});
+
+export default db;
